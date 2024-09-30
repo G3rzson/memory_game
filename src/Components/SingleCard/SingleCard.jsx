c489bca0b14f84c3aca6bdb1abdc1f674ce881c0
Gerzson <gerzonpal@gmail.com>
2024-09-30 20:41:15 +0200

    update gameplay

--- src/Components/SingleCard/SingleCard.jsx
+++ src/Components/SingleCard/SingleCard.jsx
@@ -1,24 +1,33 @@
 
-import React from 'react'
-import './SingleCard.css'
+import React, { useEffect, useState } from 'react';
+import './SingleCard.css';
 
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+    const [flipp, setFlipp] = useState('flipped');
 
-    const handleClick = ()=> {
+    const handleClick = () => {
         if (!disabled) {
-            handleChoice(card)
+            handleChoice(card); 
         }
-    }
+    };
+
+    useEffect(() => {    
+        setTimeout(() => {
+            setFlipp(''); 
+        }, 600); // 0.6s  
+    }, [flipp]);
 
     return (
         <div className='card'>
-            <div className={flipped ? "flipped" : ''}>
-                <img className='front' src={card.src} alt='card front'/>
-                <img className='back' 
+            <div className={`${flipped ? "flipped" : ''} ${flipp}`}>
+                <img className='front' src={card.src} alt='card front' />
+                <img 
+                    className='back' 
                     onClick={handleClick} 
                     src='/memory_game_React/pics/pattern.svg' 
-                    alt='card back'/>
+                    alt='card back' 
+                />
             </div>
         </div>
-    )
+    );
 }
